4886fc3b650cdb228cb2bb54cd8837e99fc81083
RubinMz <jafetzarate@outlook.com>
2015-06-14 11:13:44 -0500

    Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
   
   #Almacenamiento de imagenes con amazon s3
-  #config.paperclip_defaults = {
-  #:storage => :s3,
-  #:s3_credentials => {
-  #  :bucket => ENV['rubinmz'],
-  #  :access_key_id => ENV['AKIAI7C3QYH3CS4ZREQQ'],
-  #  :secret_access_key => ENV['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']
-  #}
-  #}
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['rubinmz'],
+      :access_key_id => ENV['AKIAI7C3QYH3CS4ZREQQ'],
+      :secret_access_key => ENV['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']
+    }
+  }
 end
